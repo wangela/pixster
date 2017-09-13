c22a18b44a1d44e0d5571f0ff9ce559624550f4a
wangela <wangela@users.noreply.github.com>
2017-09-12 23:56:56 -0700

    Load movie posters, titles, and storylines in a scrollable TableView

---
+++ pixster/Pods/Target Support Files/Pods-pixster/Pods-pixster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pixster : NSObject
+@end
+@implementation PodsDummy_Pods_pixster
+@end
---
+++ pixster/pixster/MoviesViewController.swift
@@ -0,0 +1,91 @@
+//
+//  MoviesViewController.swift
+//  pixster
+//
+//  Created by Angela Yu on 9/12/17.
+//  Copyright © 2017 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var moviesTable: UITableView!
+    
+    var moviesDict: [[String: Any]] = [[String: Any]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        moviesTable.dataSource = self
+        moviesTable.delegate = self
+        
+
+        let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")
+        var request = URLRequest(url: url!)
+        request.cachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(with: request, completionHandler: { (dataOrNil, response, error) in
+            if let data = dataOrNil {
+                
+                let dictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+                
+                print(dictionary)
+                self.moviesDict = dictionary["results"] as! [[String: Any]]
+                self.moviesTable.reloadData()
+            }
+        });
+        task.resume()
+    }
+ 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return moviesDict.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
+        let movie = moviesDict[indexPath.row]
+        let title = movie["title"] as? String
+        let storyline = movie["overview"] as? String
+        if let posterPath = movie["poster_path"] as? String {
+            let posterBaseUrl = "http://image.tmdb.org/t/p/w500"
+            let posterUrl = URL(string: posterBaseUrl + posterPath)
+            cell.posterView.setImageWith(posterUrl!)
+        }
+        else {
+            // No poster image. Can either set to nil (no image) or a default movie poster image
+            // that you include as an asset
+            cell.posterView.image = nil
+        }
+        
+        cell.titleLabel.text = title
+        cell.storylineLabel.text = storyline
+        
+        return cell
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
